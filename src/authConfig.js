97af323bf1cf6635b1beee9dd3ed705b73bb9f77
alief zafran <asafaruddin@deloitte.com>
2023-10-30 15:47:46 +0800

    tst

--- src/authConfig.js
+++ src/authConfig.js
@@ -1,6 +1,6 @@
 const authConfig = {
     authority: 'http://localhost:8080', //issuer URL
-    client_id: '237884994054324227@project1', //Replace with your client id
+    client_id: 'your-own-client-id', //Replace with your client id
     redirect_uri: 'http://localhost:3000/callback',
     response_type: 'code',
     scope: 'openid profile email',
